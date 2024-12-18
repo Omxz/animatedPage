2b8e6e41e344161810b068f82fbd9c049ff9f738
Sebastian Svensson <sebastian.svensson@livearenasports.com>
2024-12-18 10:45:14 +0100

    falling snowflakes

---
+++ animated-page/src/app/page.tsx
@@ -0,0 +1,10 @@
+import SnowAnimation from "../components/snowanimation";
+import './globals.css';
+
+export default function Home() {
+  return (
+    <main>
+      <SnowAnimation />
+    </main>
+  );
+}
